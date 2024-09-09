ff98df6a4204f6efb677bd9dd772259c79e9112a
Prieto Ezequiel <72413070+zequi-pv@users.noreply.github.com>
2024-09-09 15:24:08 -0300

    Create calculate magnitude

--- TP2Algebra/main.cpp
+++ TP2Algebra/main.cpp
@@ -1,33 +1,54 @@
 #include <iostream>
 #include "raylib.h"
 
+using namespace std;
+
+struct VecRect
+{
+	Vector3 pos;
+	Vector3 rotationAngles;
+	float magnitude;
+};
+
 void Update();
 void Draw();
+void calculateMagnitude(VecRect& vector);
 
 int main(void)
 {
 	srand(time(NULL));
 
-	const int screenWidth = 800;
-	const int screenHeight = 500;
+	const int screenWidth = 1280;
+	const int screenHeight = 720;
 	int maxDegrees = 360;
-	//const int maxPoints = 3;
 
 	Vector3 startPos = { 0.0f, 0.0f, 0.0f, };
 
-	Vector3 vectorA = { rand() % maxDegrees, rand() % maxDegrees, rand() % maxDegrees };
-	Vector3 vectorB = { 0.0f, 0.0f, 0.0f };
-	Vector3 vectorC = { 0.0f, 0.0f, 0.0f };
+	VecRect vectorA;
+	vectorA.pos = startPos;
+	vectorA.rotationAngles = { static_cast<float>(rand() % maxDegrees), static_cast<float>(rand() % maxDegrees), static_cast<float>(rand() % maxDegrees) };
+	vectorA.magnitude = 1;
 
-	//Vector3 points[maxPoints] = { a, b, c };
+	VecRect vectorB;
+	vectorB.pos = startPos;
+	vectorB.rotationAngles = { 0.0f, 0.0f, 0.0f };
+	vectorB.magnitude = 1;
+
+	VecRect vectorC;
+	vectorC.pos = startPos;
+	vectorC.rotationAngles = { 0.0f, 0.0f, 0.0f };
+	vectorC.magnitude = 1;
 
 	InitWindow(screenWidth, screenHeight, "TP2 Algebra");
 
 	SetTargetFPS(60);
 
+	calculateMagnitude(vectorA);
+
 	while (!WindowShouldClose())
 	{
 		Update();
+		cout << vectorA.magnitude << endl;
 		Draw();
 	}
 
@@ -38,7 +59,7 @@ int main(void)
 
 void Update()
 {
-
+	
 }
 
 void Draw()
@@ -49,4 +70,9 @@ void Draw()
 
 
 	EndDrawing();
+}
+
+void calculateMagnitude(VecRect& vector) 
+{
+	vector.magnitude = sqrt(pow(vector.rotationAngles.x, 2.0f) + pow(vector.rotationAngles.y, 2.0f) + pow(vector.rotationAngles.z, 2.0f));
 }
